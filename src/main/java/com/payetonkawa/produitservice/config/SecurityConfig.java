41ce7fd74002297ef28d3d3bf6ff2fd798974d55
Gwenaëlle DEPOORTER <gwenaelle8depoorter@corp.cordongroup.com>
2025-06-05 10:57:11 +0200

    Ajout du Security Config

--- src/main/java/com/payetonkawa/produitservice/config/SecurityConfig.java
+++ src/main/java/com/payetonkawa/produitservice/config/SecurityConfig.java
@@ -1,4 +1,16 @@
 package com.payetonkawa.produitservice.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
+        http.authorizeHttpRequests(authz ->authz
+                .anyRequest().permitAll())
+                .csrf().disable()
+                .headers().frameOptions().disable();
+        return http.build();
+    }
 }
